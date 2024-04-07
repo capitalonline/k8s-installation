323c3616ede88d1d2392dae8b5cd9843a610dad9
Honglin Feng <tifayuki@gmail.com>
2024-04-07 14:46:47 +0800

    added init script to initialize all the nodes

--- master001.sh
+++ master001.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
 
-# 添加阿里云的APT源
-cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
-deb http://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
-deb-src http://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
-EOF
-
-# 安装依赖
-sudo apt-get update && sudo apt-get install -y apt-transport-https curl
-
-# 添加Kubernetes GPG key
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-
-# 更新并安装Kubernetes组件
-sudo apt-get update && sudo apt-get install -y kubeadm kubelet kubectl
-
-# 禁用交换分区
-sudo sed -i '/ swap / s/^$.*$$/#\1/g' /etc/fstab
-sudo swapoff -a
+./init.sh
 
 # 初始化Kubernetes master节点
 sudo kubeadm init --kubernetes-version=1.29.0 --pod-network-cidr=10.244.0.0/16
--- master002.sh
+++ master002.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+./init.sh
+
 sudo kubeadm join <master1_ip>:6443 --token <token> --discovery-token-ca-cert-hash sha256:<hash>
